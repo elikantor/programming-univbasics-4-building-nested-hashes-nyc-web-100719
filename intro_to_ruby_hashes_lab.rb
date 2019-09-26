fd5b121e21bae979bdbe289cc5b1493b90526147
Eli Kantor <eli.kantor@outlook.com>
2019-09-26 15:51:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,10 +22,10 @@ def monopoly_with_third_tier
 	      :four => 4
 	      },
 	    :rent_in_dollars => {
-	      :names => 1,
 	      :one_piece_owned => 25,
 	      :two_pieces_owned => 50,
-	      :three_pieces_owned => 100
+	      :three_pieces_owned => 100,
+	      :four_pieces_owned => 200
 	      }
 	   }
 	}
