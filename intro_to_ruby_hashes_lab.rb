f5ca89f6ab19094a6777eed99fe0eeb6875c10b4
Eli Kantor <eli.kantor@outlook.com>
2019-09-26 16:03:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,8 +37,8 @@ def monopoly_with_fourth_tier
 	    :names => {
 	      :reading_railroad => {"mortgage_value"=>"$100"},
 	      :pennsylvania_railroad => {"mortgage_value"=>"$200"},
-	      :b_and_o_railroad => {},
-	      :shortline_railroad => {}
+	      :b_and_o_railroad => {"mortgage_value"=>"$300"},
+	      :shortline_railroad => {"mortgage_value"=>"$400"}
 	      },
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
